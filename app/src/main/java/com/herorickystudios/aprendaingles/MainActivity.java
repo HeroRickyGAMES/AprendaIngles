9ef43ad1950a799cb2e5c6e62b4ecb05e63898ad
HeroRickyGAMES <ricojn9@gmail.com>
2022-01-30 19:47:21 -0300

    Adicionando anuncios e iniciando o lançamento na Google Play Store!

--- app/src/main/java/com/herorickystudios/aprendaingles/MainActivity.java
+++ app/src/main/java/com/herorickystudios/aprendaingles/MainActivity.java
@@ -7,17 +7,29 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.herorickystudios.aprendaingles.fragments.BichosFragment;
 import com.herorickystudios.aprendaingles.fragments.NumerosFragment;
 import com.herorickystudios.aprendaingles.fragments.VogaisFragment;
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+import com.google.android.gms.ads.AdRequest;
+
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private SmartTabLayout smartTabLayout;
     private ViewPager viewPager;
+    private AdView AdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +54,41 @@ public class MainActivity extends AppCompatActivity {
 
         //remover sombra da actionbar
         getSupportActionBar().setElevation(0);
+
+
+
+        //inicialização dos ads
+        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+        // values/strings.xml.
+
+        AdView adView = new AdView(this);
+
+        adView.setAdSize(AdSize.BANNER);
+
+        adView.setAdUnitId("ca-app-pub-1777154185753322/3169983460");
+// TODO: Add adView to your view hierarchy.
+        // Set your test devices. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+        // to get test ads on this device."
+        MobileAds.setRequestConfiguration(
+                new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+                        .build());
+
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(
+                this,
+                new OnInitializationCompleteListener() {
+                    @Override
+                    public void onInitializationComplete(InitializationStatus status) {}
+                });
+
+
+        AdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        AdView.loadAd(adRequest);
+
+
 
     }
 }
